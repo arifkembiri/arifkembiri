cd272fcfd447f2668a7879a27bd094322c8b0d77
arifkembiri <110958613+arifkembiri@users.noreply.github.com>
2024-01-18 13:43:34 +0700

    Add files via upload

---
+++ fireConfig.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyAfR488P55GGhkLR5sQ4agX88q4CseLmBo",
+  authDomain: "arifkembiri-ae0e7.firebaseapp.com",
+  databaseURL: "https://arifkembiri-ae0e7-default-rtdb.firebaseio.com",
+  projectId: "arifkembiri-ae0e7",
+  storageBucket: "arifkembiri-ae0e7.appspot.com",
+  messagingSenderId: "699944213212",
+  appId: "1:699944213212:web:37833bf7b961ad62bab219",
+  measurementId: "G-RL0D65F80T"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
